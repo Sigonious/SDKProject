f38d1c913fcfca1a90a84a86c7fd81d3e37ebc80
bm05209 <bm05209@georgiasouthern.edu>
2016-11-03 00:55:18 -0400

    Minor registerPage changes
    Added password requirements, comments, and formatting

--- registerPage.php
+++ registerPage.php
@@ -1,21 +1,18 @@
 <!DOCTYPE html>
 
 <!--
-Add password strengths
-Check if email is in database before insertion
+//Not compatible with main database in this version, only test database
+
 !-->
-<!html>
+<html>
 	<head>
 		<title>Register</title>
-		<meta charset="utf-8">
-		<link rel = "stylesheet" type = "text/css" href = "SDKstyle.css">
-		<script src="accountValidate.js"></script>
-	</head>
+			<meta charset="utf-8">
+			<link rel = "stylesheet" type = "text/css" href = "SDKstyle.css">
+	</head>	
+	<body>	
 	
-	<body>
-	
-	
-	<?php echo "ayy lmao";
+	<?php
 	
 	$user = 'root';
 	$pass = '';
@@ -24,6 +21,7 @@ Check if email is in database before insertion
 	$db = new mysqli('localhost',$user,$pass,$db) or die("Unable to connect");
 
 	
+	//Checks if forms are empty and meets requirements
 	
 	$firstNameErr = $lastNameErr = $emailErr = $checkEmailErr = $pwd1Err = $pwd2Err ="";
 
@@ -31,62 +29,75 @@ Check if email is in database before insertion
 	
 	if($_SERVER["REQUEST_METHOD"] == "POST"){
 	
-	if(empty($_POST["firstName"])){
-		$firstNameErr = "First Name is required";
-		$Err = true;
-	} else {
-		$firstName = test_input($_POST["firstName"]);
-		if (!preg_match("/^[a-zA-Z ]*$/",$firstName)) {
-			$firstNameErr = "Invalid Name"; 
+		//First Name
+		if(empty($_POST["firstName"])){
+			$firstNameErr = "First Name is required";
 			$Err = true;
+		} else {
+			$firstName = test_input($_POST["firstName"]);
+			if (!preg_match("/^[a-zA-Z ]*$/",$firstName)) {
+				$firstNameErr = "Invalid Name"; 
+				$Err = true;
+			}
+		}
+	
+		//Last Name
+		if(empty($_POST["lastName"])){
+			$lastNameErr = "Last Name is required";
+			$Err = true;
+		} else {
+			$lastName = test_input($_POST["lastName"]);
+			if (!preg_match("/^[a-zA-Z ]*$/",$lastName)) {
+				$lastNameErr = "Invalid Name"; 
+				$Err = true;
+			}
+		}
+	
+		//Email
+		if(empty($_POST["email"])){
+			$emailErr = "Email is required";
+			$Err = true;
+		} else {
+			$email = test_input($_POST["email"]);
+			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+				$emailErr = "Invalid Email"; 
+				$Err = true;
 		}
 	}
 	
-	if(empty($_POST["lastName"])){
-		$lastNameErr = "Last Name is required";
-		$Err = true;
-	} else {
-	$lastName = test_input($_POST["lastName"]);
-	if (!preg_match("/^[a-zA-Z ]*$/",$lastName)) {
-			$lastNameErr = "Invalid Name"; 
+		//Email Check
+		if(empty($_POST["checkEmail"])){
+			$checkEmailErr = "Email confirmation is required";
+			$Err = true;
+			} elseif($_POST["email"] == $_POST["checkEmail"]) {
+				$checkEmail = test_input($_POST["checkEmail"]);
+			} else {
+				$checkEmailErr = "Email does not match";
+				$Err = true;
+			}
+	
+		//Password
+		//at least 8 characters and contains a letter
+		if(empty($_POST["pwd1"])){
+			$pwd1Err = "Password is required";
+			$Err = true;
+		} elseif (!preg_match('/^(?=[a-z])[a-zA-Z]{8,}$/', $pwd1)){
+			$pwd1Err = "Password must be 8 characters and contain a letter"
+			$Err = true;
+		} else {
+			$pwd1 = test_input($_POST["pwd1"]);
+		}
+	
+		//Password Check
+		if(empty($_POST["pwd2"])){
+			$pwd2 = "Password confirmation is required";
+			$Err = true;
+		} elseif($_POST["pwd1"] == $_POST["pwd2"]){
+			$pwd2 = test_input($_POST["pwd2"]);
+		} else {
+			$pwd2Err = "Password does not match";
 			$Err = true;
 		}
-	}
-	
-	if(empty($_POST["email"])){
-		$emailErr = "Email is required";
-		$Err = true;
-	} else {
-		$email = test_input($_POST["email"]);
-		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-			$emailErr = "Invalid Email"; 
-			$Err = true;
-		}
-	}
-	
-	if(empty($_POST["checkEmail"])){
-		$checkEmailErr = "Email confirmation is required";
-		$Err = true;
-	} elseif($_POST["email"] == $_POST["checkEmail"]) {
-	$checkEmail = test_input($_POST["checkEmail"]);
-	} else {
-		$checkEmailErr = "Email does not match";
-	$Err = true;}
-	
-	if(empty($_POST["pwd1"])){
-		$pwd1Err = "Password is required";
-		$Err = true;
-	} else {
-	$pwd1 = test_input($_POST["pwd1"]);
-	}
-	
-	if(empty($_POST["pwd2"])){
-		$pwd2 = "Password confirmation is required";
-		$Err = true;
-	} elseif($_POST["pwd1"] == $_POST["pwd2"]){
-		$pwd2 = test_input($_POST["pwd2"]);
-	} else {$pwd2Err = "Password does not match";
-	$Err = true;}
 	
 	}
 	$firstName = mysql_real_escape_string($firstName);
@@ -94,6 +105,7 @@ Check if email is in database before insertion
 	$email = mysql_real_escape_string($email);
 	$pwd1 = mysql_real_escape_string($pwd1);
 	
+	//Inserts values from form into database if there are no errors; Err=true
 	if($Err != true){
 	$sql = "INSERT INTO users (firstName, lastName, email, password)
 	VALUES('$firstName','$lastName','$email','$pwd1' )";
