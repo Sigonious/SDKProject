da1a79bafac2799e34b93e06dc9093e17e96d835
Adam Murr <adamcmurr@gmail.com>
2017-01-06 21:30:30 -0500

    Delete config.php

--- php/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-	define('dbserver','localhost');
-	define('dbusername','root');
-	define('dbpassword','root');
-	define('dbdatabase','test');
-	
-	//connect to DB
-	$conn = new mysqli(dbserver, dbusername, dbpassword, dbdatabase);
-?>
